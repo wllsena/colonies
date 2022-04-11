0767e50d10c372995f8c4f2f1e0e4686024a23af
Will Sena <wllsena@protonmail.com>
2022-04-11 12:28:21 -0300

    Adicionando pause e resume

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include "src/structure_and_print.cpp"
+#include <iostream>
+#include <thread>
 #include <unistd.h>
 #include <vector>
 
 using namespace std;
+
+bool paused = false;
+
+void control() {
+  while (true) {
+    if (cin.get())
+      paused = not paused;
+  }
+}
+
+void play(World *world, int n_iters, int sleep_time) {
+  clear_console();
+  cout << "Press ENTER to start, pause and resume." << endl;
+
+  for (int i = 0; i != n_iters;) {
+    if (paused) {
+      world->play();
+      clear_console();
+      print_iter(i);
+      print_world(world);
+      print_values(world);
+      usleep(sleep_time);
+      i += 1;
+    }
+  }
+}
 
 int main() {
   int threads = 1;
@@ -25,20 +53,9 @@ int main() {
 
   World *world = new World(x, y, foods, colonies);
 
-  vector<int> amounts;
+  thread t_control(control);
+  thread t_play(play, world, n_iters, sleep_time);
 
-  clear_console();
-  print_iter(0);
-  print_world(world);
-  print_values(world);
-  usleep(sleep_time);
-
-  for (int i = 1; i != n_iters; i++) {
-    world->play();
-    clear_console();
-    print_iter(i);
-    print_world(world);
-    print_values(world);
-    usleep(sleep_time);
-  }
+  t_control.join();
+  t_play.join();
 }
